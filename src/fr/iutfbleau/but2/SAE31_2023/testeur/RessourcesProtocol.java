9cc0132727364c5deabfe6f2c9df1e8a4f6f307d
Yanis <139372309+TheSinay@users.noreply.github.com>
2023-11-09 20:53:16 +0100

    protocol dynamique finis

--- src/fr/iutfbleau/but2/SAE31_2023/testeur/RessourcesProtocol.java
+++ src/fr/iutfbleau/but2/SAE31_2023/testeur/RessourcesProtocol.java
@@ -1,32 +1,68 @@
 import java.util.ArrayList;
 
 public class RessourcesProtocol {
-    private Object[] protocol ;
+    
     private String[] protocolNom;
+    private String[] protocolDescription;
+    private int[] protocolID;
+    private int taille;
 
-    public RessourcesProtocol(ArrayList<Object> données) {
-        protocol = new Object[données.size()];
-        protocolNom = new String[données.size()];
+    public RessourcesProtocol(ArrayList<Object> donnees) {
+        taille = donnees.size();
+        protocolNom = new String[taille];
+        protocolDescription = new String[taille];
+        protocolID = new int[taille];
 
         int i = 0;
-        for (Object element : données) {
-            protocol[i] = element ;
-            if (element instanceof String) {
-                protocolNom[i] = (String) element;
+        for (Object elements : donnees) {
+            if (elements instanceof ArrayList) {
+                ArrayList<Object> element = (ArrayList<Object>) elements;
+                if (element.get(1) instanceof String && element.get(2) instanceof String && element.get(0) instanceof Integer) {
+                    protocolNom[i] = (String) element.get(1);
+                    protocolDescription[i] = (String) element.get(2);
+                    protocolID[i] = (int) element.get(0);
+                } else {
+                    // Gérer le cas où l'élément n'est pas une chaîne
+                    protocolNom[i] = "Non défini";
+                    protocolDescription[i] = "Non défini";
+                }
             } else {
-                // Gérer le cas où l'élément n'est pas une chaîne
                 protocolNom[i] = "Non défini";
+                protocolDescription[i] = "Non défini";
             }
             i++;
         }
     }
 
-    public Object[] getProtocol() {
-        return protocol;
+    public String[] getProtocolNom() {
+        return protocolNom;
+    }
+    
+
+    
+
+    public String getProtocolDescription(String nomprotocol) {
+        
+        for (int i = 0;i < taille  ; i++) {
+            if (protocolNom[i] == nomprotocol) {
+                
+                return protocolDescription[i];
+            }
+        }
+        return "il n'y a pas de description";
     }
 
-    public String[] getProtocolNom() {
-        return protocolNom ;
-    
+     public int getProtocolID(String nomprotocol) {
+        int id = 0;
+        for (int i = 0;i < taille  ; i++) {
+            if (protocolNom[i] == nomprotocol) {
+                id = protocolID[i];
+            }
+        }
+        return id;
+    }
+
+    public String getFirstDescription() {
+        return protocolDescription[0];
     }
 }
