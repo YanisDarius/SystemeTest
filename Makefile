b0049a881992a3f9d9a92f820bfcde0596c82403
Yanis <139372309+TheSinay@users.noreply.github.com>
2023-11-15 11:27:54 +0100

    papa

--- Makefile
+++ Makefile
@@ -2,12 +2,22 @@ build:
 	mkdir -p build
 	javac src/fr/iutfbleau/but2/SAE31_2023/*.java -d build
 	
+build testeur:
+	mkdir -p build
+	javac src/fr/iutfbleau/but2/SAE31_2023/testeur/*.java -d build
+
+run testeur:
+	java -cp "src/fr/iutfbleau/but2/SAE31_2023/mariadb-java-client-1.8.0.jar:build:" Main
+
+	
 run:
 	java -cp "src/fr/iutfbleau/but2/SAE31_2023/mariadb-java-client-1.8.0.jar:build:" Main
-start:
+
+clean:
 	rm -f build/*class
+start:
+	clean
 	mkdir -p build
 	javac src/fr/iutfbleau/but2/SAE31_2023/*.java -d build
 	java -cp "src/fr/iutfbleau/but2/SAE31_2023/mariadb-java-client-1.8.0.jar:build:" Main
-clean:
-	rm -f build/*class
+
---
+++ src/fr/iutfbleau/but2/SAE31_2023/testeur/Ecran.java
@@ -0,0 +1,47 @@
+import java.awt.CardLayout;
+import java.awt.Component;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Ecran {
+
+    private JFrame frame = new JFrame("test");
+    private JPanel cardPanel;
+    private CardLayout cardLayout;
+
+    public Ecran() {
+        cardPanel = new JPanel();
+        cardLayout = new CardLayout();
+        cardPanel.setLayout(cardLayout);
+
+        // definition de l ecran grace a option ecran
+        EcranOption ecran = new EcranOption();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(ecran.getWidth(), ecran.getHeight());
+
+    }
+
+    public JFrame Frame() {
+        return frame;
+    }
+
+    public CardLayout getCardLayout() {
+        return cardLayout;
+    }
+
+    public JPanel getCardJPanel() {
+        return cardPanel;
+    }
+
+    public void ajouterEcran(Component ajoutDesComponent, String nomComponent) {
+        cardPanel.add(ajoutDesComponent, nomComponent);
+    }
+
+    public void revelationEcran() {
+
+        this.Frame().add(cardPanel);
+        this.Frame().setVisible(true);
+    }
+
+}
